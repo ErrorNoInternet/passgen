18ddbc2ffc0cc91e8af38a82f57a50a181c8d148
ErrorNoInternet <error.nointernet@gmail.com>
2023-07-22 18:31:54 +0800

    RUST GO BRRR

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-const ITEMS: &[&str] = &["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+use std::io::{BufWriter, StdoutLock, Write};
 
-fn generate(level: usize, max_level: usize, prefix: &str) {
+const ITEMS: &[&str] = &["a", "b", "c", "d", "e", "f", "g", "h"];
+
+fn generate(stdout_bufwriter: &mut BufWriter<StdoutLock>, level: usize, prefix: &mut String) {
     if level == 0 {
-        println!("{}", prefix);
+        stdout_bufwriter.write(prefix.as_bytes()).unwrap();
+        stdout_bufwriter.write(b"\n").unwrap();
     } else {
-        for i in 0..ITEMS.len() {
-            generate(level - 1, max_level, &(prefix.to_owned() + ITEMS[i]));
+        for &item in ITEMS {
+            let previous_len = prefix.len();
+            prefix.push_str(item);
+            generate(stdout_bufwriter, level - 1, prefix);
+            prefix.truncate(previous_len);
         }
     }
 }
 
 fn main() {
-    for level in 0..=ITEMS.len() {
-        generate(level, ITEMS.len(), "");
+    let mut stdout_bufwriter = BufWriter::new(std::io::stdout().lock());
+    let mut prefix = String::with_capacity(256);
+    for i in 0..=ITEMS.len() {
+        generate(&mut stdout_bufwriter, i, &mut prefix)
     }
 }
