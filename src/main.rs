2625c797a9e2ae8d4b11646317e47698c5fa7019
ErrorNoInternet <error.nointernet@gmail.com>
2023-07-22 20:52:34 +0800

    passgen 0.1.0
    Replaced heap String with stack ArrayString && added CLI support (along with a flag that calculates how many lines and how many bytes the output will approximately take up)

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,71 @@
+use arrayvec::ArrayString;
+use clap::{ArgAction, Parser};
 use std::io::{BufWriter, StdoutLock, Write};
 
-const ITEMS: &[&str] = &["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Arguments {
+    /// Add a keyword to the list
+    #[arg(short, long, action = ArgAction::Append, required = true)]
+    keyword: Vec<String>,
 
-fn generate(stdout_bufwriter: &mut BufWriter<StdoutLock>, level: usize, prefix: &mut String) {
+    /// Calculate how many passwords there will be
+    #[arg(short, long, required = false)]
+    calculate_size: bool,
+}
+
+fn generate(
+    stdout_bufwriter: &mut BufWriter<StdoutLock>,
+    keywords: &Vec<&str>,
+    level: usize,
+    prefix: &mut ArrayString<256>,
+) {
     if level == 0 {
         stdout_bufwriter.write_all(prefix.as_bytes()).unwrap();
         stdout_bufwriter.write_all(b"\n").unwrap();
     } else {
-        for item in ITEMS {
-            let previous_len = prefix.len();
+        for item in keywords {
+            let previous_length = prefix.len();
             prefix.push_str(item);
-            generate(stdout_bufwriter, level - 1, prefix);
-            prefix.truncate(previous_len);
+            generate(stdout_bufwriter, keywords, level - 1, prefix);
+            prefix.truncate(previous_length);
         }
     }
 }
 
+fn calculate_combinations(n: u128, i: u32) -> u128 {
+    if i == 1 {
+        return n.into();
+    } else {
+        n.pow(i) + calculate_combinations(n, i - 1)
+    }
+}
+
 fn main() {
+    let arguments = Arguments::parse();
+    let keywords: Vec<&str> = arguments.keyword.iter().map(|item| item.as_str()).collect();
+
+    if arguments.calculate_size {
+        let lines = calculate_combinations(keywords.len() as u128, keywords.len() as u32) + 1;
+        let average_length =
+            keywords.iter().map(|item| item.len()).sum::<usize>() as f64 / keywords.len() as f64;
+        let mut estimated_bytes = 1.0;
+        for i in 1..=keywords.len() {
+            let current_lines = keywords.len().pow(i as u32) as f64;
+            estimated_bytes += current_lines + current_lines * (average_length * i as f64)
+        }
+        println!(
+            "keywords: {}\n\nline count: {}\nestimated bytes: {}",
+            keywords.len(),
+            lines,
+            estimated_bytes,
+        );
+        return;
+    }
+
     let mut stdout_bufwriter = BufWriter::new(std::io::stdout().lock());
-    let mut prefix = String::with_capacity(256);
-    for i in 0..=ITEMS.len() {
-        generate(&mut stdout_bufwriter, i, &mut prefix)
+    let mut prefix = ArrayString::<256>::new();
+    for i in 0..=keywords.len() {
+        generate(&mut stdout_bufwriter, &keywords, i, &mut prefix)
     }
 }
