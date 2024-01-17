2b61acc8470a77065075571542388bb786ceaeb3
ErrorNoInternet <errornointernet@envs.net>
2024-01-17 15:58:14 +0800

    refactor(zig): camelCase

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -7,40 +7,40 @@ const BUFFER_SIZE = 8192;
 const PREFIX_SIZE = 512;
 
 var buffer: [BUFFER_SIZE]u8 = undefined;
-var buffer_usage: usize = 0;
+var bufferUsage: usize = 0;
 
 fn generate(
     allocator: Allocator,
     keywords: [][]const u8,
-    keyword_count: u8,
-    keyword_lengths: []const u8,
+    keywordCount: u8,
+    keywordLengths: []const u8,
     prefix: *[PREFIX_SIZE]u8,
-    prefix_length: usize,
+    prefixLength: usize,
     level: u8,
 ) !void {
-    if (buffer_usage >= BUFFER_SIZE - prefix_length) {
-        _ = try stdout.writeAll(buffer[0..buffer_usage]);
-        buffer_usage = 0;
+    if (bufferUsage >= BUFFER_SIZE - prefixLength) {
+        _ = try stdout.writeAll(buffer[0..bufferUsage]);
+        bufferUsage = 0;
     }
     if (level == 0) {
-        prefix[prefix_length] = '\n';
-        std.mem.copy(u8, buffer[buffer_usage..], prefix[0 .. prefix_length + 1]);
-        buffer_usage += prefix_length + 1;
+        prefix[prefixLength] = '\n';
+        std.mem.copy(u8, buffer[bufferUsage..], prefix[0 .. prefixLength + 1]);
+        bufferUsage += prefixLength + 1;
     } else {
         var i: u8 = 0;
-        while (i < keyword_count) : (i += 1) {
-            const keyword_length = keyword_lengths[i];
-            std.mem.copy(u8, prefix[prefix_length..], keywords[i][0..keyword_length]);
-            _ = try generate(allocator, keywords, keyword_count, keyword_lengths, prefix, prefix_length + keyword_length, level - 1);
+        while (i < keywordCount) : (i += 1) {
+            const keywordLength = keywordLengths[i];
+            std.mem.copy(u8, prefix[prefixLength..], keywords[i][0..keywordLength]);
+            _ = try generate(allocator, keywords, keywordCount, keywordLengths, prefix, prefixLength + keywordLength, level - 1);
         }
     }
 }
 
-fn calculate_combinations(n: u64, i: u32) u64 {
+fn calculateCombinations(n: u64, i: u32) u64 {
     if (i == 1) {
         return n;
     } else {
-        return math.pow(u64, n, i) + calculate_combinations(n, i - 1);
+        return math.pow(u64, n, i) + calculateCombinations(n, i - 1);
     }
 }
 
@@ -52,9 +52,9 @@ pub fn main() !void {
     var keywords = try std.ArrayList([]const u8).initCapacity(allocator, 64);
     defer keywords.deinit();
 
-    var keyword_lengths: [64]u8 = undefined;
-    var do_calculate_combinations = false;
-    var keyword_count: u8 = 0;
+    var keywordLengths: [64]u8 = undefined;
+    var doCalculateCombinations = false;
+    var keywordCount: u8 = 0;
 
     const argv = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, argv);
@@ -66,40 +66,40 @@ pub fn main() !void {
         }
 
         if (std.mem.eql(u8, "-c", arg)) {
-            do_calculate_combinations = true;
+            doCalculateCombinations = true;
         } else {
             try keywords.append(arg);
-            keyword_lengths[keyword_count] = @truncate(arg.len);
-            keyword_count += 1;
+            keywordLengths[keywordCount] = @truncate(arg.len);
+            keywordCount += 1;
         }
     }
-    if (keyword_count == 0) {
+    if (keywordCount == 0) {
         try stdout.print("no keywords specified!\n", .{});
         return;
     }
 
-    if (do_calculate_combinations) {
-        const lines = calculate_combinations(keyword_count, @as(u32, keyword_count)) + 1;
-        var average_length: f64 = 0;
+    if (doCalculateCombinations) {
+        const lines = calculateCombinations(keywordCount, @as(u32, keywordCount)) + 1;
+        var averageLength: f64 = 0;
         for (keywords.items) |keyword| {
-            average_length += @as(f64, @floatFromInt(keyword.len));
+            averageLength += @as(f64, @floatFromInt(keyword.len));
         }
-        average_length /= @as(f64, @floatFromInt(keyword_count));
+        averageLength /= @as(f64, @floatFromInt(keywordCount));
         var bytes: f64 = 1.0;
         var i: u32 = 1;
-        while (i <= keyword_count) : (i += 1) {
+        while (i <= keywordCount) : (i += 1) {
             const ii = @as(f64, @floatFromInt(i));
-            const current_lines = math.pow(f64, @as(f64, @floatFromInt(keyword_count)), ii);
-            bytes += current_lines + current_lines * (average_length * ii);
+            const currentLines = math.pow(f64, @as(f64, @floatFromInt(keywordCount)), ii);
+            bytes += currentLines + currentLines * (averageLength * ii);
         }
-        try stdout.print("keywords: {}\n\nline : {}\nbytes: {d}\n", .{ keyword_count, lines, bytes });
+        try stdout.print("keywords: {}\n\nline : {}\nbytes: {d}\n", .{ keywordCount, lines, bytes });
     } else {
         var prefix: [PREFIX_SIZE]u8 = undefined;
-        for (0..keyword_count + 1) |i| {
-            _ = try generate(allocator, keywords.items, keyword_count, &keyword_lengths, &prefix, 0, @truncate(i));
+        for (0..keywordCount + 1) |i| {
+            _ = try generate(allocator, keywords.items, keywordCount, &keywordLengths, &prefix, 0, @truncate(i));
         }
     }
-    if (buffer_usage > 0) {
-        _ = try stdout.writeAll(buffer[0..buffer_usage]);
+    if (bufferUsage > 0) {
+        _ = try stdout.writeAll(buffer[0..bufferUsage]);
     }
 }
